901ee6bc385d2e83d7684c0611f2421c2b6b367b
DESKTOP-9CAQJI2\zst <zhangst5784@gmail.com>
2024-07-28 11:04:45 +0800

    feat: 配置调整

--- src/main/java/com/zst/dfs/storage/StorageConfiguration.java
+++ src/main/java/com/zst/dfs/storage/StorageConfiguration.java
@@ -1,7 +1,7 @@
 package com.zst.dfs.storage;
 
 import com.zst.dfs.storage.metadata.MetadataService;
-import com.zst.dfs.storage.metadata.filesystem.FileSystemMetadataService;
+import com.zst.dfs.storage.metadata.db.DbMetadataService;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 public class StorageConfiguration {
     @Bean
     public MetadataService metadataService() {
-        return new FileSystemMetadataService();
+        return new DbMetadataService();
     }
 
     @Bean
