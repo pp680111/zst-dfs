4c72c3a36dface193dc30eac9592d1a5d70fb894
zhangshutao <zhangshutao@ava.com.cn>
2024-07-29 19:44:28 +0800

    fix: 整理调整Configuration类的代码

--- src/main/java/com/zst/dfs/cluster/configuration/DfsConfiguration.java
+++ src/main/java/com/zst/dfs/cluster/configuration/DfsConfiguration.java
@@ -1,15 +1,31 @@
-package com.zst.dfs.cluster.db;
+package com.zst.dfs.cluster.configuration;
 
+import com.zst.dfs.cluster.db.ClusterNodeManager;
+import com.zst.dfs.cluster.db.NodeProperties;
 import com.zst.dfs.cluster.db.clusternode.ClusterNodeService;
 import com.zst.dfs.cluster.db.filereplica.FileReplicaService;
+import com.zst.dfs.storage.FileSystemStorageProperties;
+import com.zst.dfs.storage.FileSystemStorageService;
+import com.zst.dfs.storage.StorageService;
 import com.zst.dfs.storage.metadata.MetadataService;
+import com.zst.dfs.storage.metadata.db.DbMetadataService;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({NodeProperties.class})
-public class DbClusterConfiguration {
+@EnableConfigurationProperties({NodeProperties.class, FileSystemStorageProperties.class})
+public class DfsConfiguration {
+
+    @Bean
+    public MetadataService metadataService() {
+        return new DbMetadataService();
+    }
+
+    @Bean
+    public StorageService storageService() {
+        return new FileSystemStorageService();
+    }
 
     @Bean
     public ClusterNodeManager clusterNodeManager(ClusterNodeService clusterNodeService,
